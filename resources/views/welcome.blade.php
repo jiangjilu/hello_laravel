285bd25a2f2eb375c1b7a85695affc7489b4c3bc
Qinphp <qinphp@qq.com>
2018-11-02 07:31:35 +0000

    Add --- Hello Laravel ---

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello, Laravel!
+                    --- Hello, Laravel ---
                 </div>
 
                 <div class="links">
